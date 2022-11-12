2e32e524855cbe9a81ad31c5e21fbc3e89f9e631
Suraj Pisal <surajpisal113@gmail.com>
2022-11-12 23:27:29 +0530

    created home view with singnup and login

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
-from re import TEMPLATE
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
-# from src.model import spell_number
 from pathlib import Path
 import os
+
+# servce static files
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
 
@@ -13,10 +15,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'app\\templates')
 
 templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
+# serve static files
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 
 @app.get("/")
 def form_post(request: Request):
     result = "Type a number"
-    return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
-
-
+    return templates.TemplateResponse('index.html', context={'request': request, 'result': result})
