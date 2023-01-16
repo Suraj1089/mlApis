8dcbb04494891b46e558245c6661913516980fb2
Suraj Pisal <surajpisal113@gmail.com>
2023-01-16 18:24:29 +0530

    change in project structure deployment setup created to deploy on render

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from blog import  models
+from blog import models
 from blog.database import engine
 from blog.routers import blog, user, authentication
+
 
 app = FastAPI()
 
